bcd8ab75e7e6d3ef91e7bf1866d82263728df7f1
Mariana Ortiz <122634035+mariortizm@users.noreply.github.com>
2023-10-19 12:41:22 -0500

    Subo el laboratorio 5

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,22 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    del df['Unnamed: 0']
+    df = df.dropna().drop_duplicates()
+
+    df['sexo'] = df['sexo'].apply(str.lower).str.capitalize()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower).str.capitalize()
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ')).str.capitalize()
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' ')).str.capitalize()
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ')).str.capitalize()
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '', regex=False).str.replace(' ', '').astype(float)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], format='mixed', dayfirst=True)
+    
+    df = df.drop_duplicates().dropna()
+    df.reset_index(drop = True)
 
     return df
